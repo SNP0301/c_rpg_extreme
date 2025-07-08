b395ed3e357338a6b49ea1c8c886cd7808acb15f
SNP0301 <studiosnp0301@gmail.com>
2025-07-08 17:10:29 +0900

    map, monster, player header

--- include/map.h
+++ include/map.h
@@ -1,3 +1,16 @@
-git add .
-git commit -m '아무  메시지'
-git push origin master
+#ifndef MAP_H
+#define MAP_H
+
+#define MAX_N 100
+#define MAX_M 100
+
+extern int N, M;
+extern char map[MAX_N][MAX_M];
+extern char original_map[MAX_N][MAX_M];
+extern int start_x, start_y;
+
+void load_map();
+void print_map(int px, int py);
+int can_move(int x, int y);
+
+#endif
